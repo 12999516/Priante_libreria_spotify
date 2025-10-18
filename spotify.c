55ae003023f4ad98a5b477916641800f095f0e64
12999516 <12999516@itisrossi.vi.it>
2025-10-18 15:54:01 +0200

    ended

--- spotify.c
+++ spotify.c
@@ -12,33 +12,53 @@ int main() {
     struct song *songs = (struct song *)malloc(sizeof(struct song) * 4);
     int dec;
     int con = 0;
+    int c;
 
     do
     {
-        printf("vuoi aggiungere una canzone? 1) si, 2) no");
+        printf("cosa vuoi fare: 0) esci, 1) aggiungi canzone, 2) visualizza libreria");
         scanf("%d", &dec);
-        printf("dimmi il titolo");
-        scanf("%s", songs[con].nome);
-        printf("dimmi l'artista");
-        scanf("%s", songs[con].artista);
-        printf("dimmi i minuti");
-        scanf("%s", songs[con].minuti);
-        printf("dimmi i secondi");
-        scanf("%s", songs[con].secondi);
-        con++;
 
-        if (con == sizeof(songs) / sizeof(struct song)) {
-            realloc(songs, sizeof(songs) * 2);
+        if (dec  == 1) {
+            songs[con].nome = malloc(sizeof(char) * 100);
+            songs[con].artista = malloc(sizeof(char) * 100);
+            printf("dimmi il titolo");
+            fflush(stdin);
+            scanf("%s", songs[con].nome);
+            printf("dimmi l'artista");
+            fflush(stdin);
+            scanf("%s", songs[con].artista);
+            printf("dimmi i minuti");
+            fflush(stdin);
+            scanf("%d", &songs[con].minuti);
+            printf("dimmi i secondi");
+            fflush(stdin);
+            scanf("%d", &songs[con].secondi);
+            con++;
+
+
+            if (con == sizeof(songs) / sizeof(struct song)) {
+                struct song *temp = realloc(songs, sizeof(songs) * 2);
+                songs = temp;
+            }
+
+            printf("vuoi fare altro? 1) si 2) no");
+            scanf("%d", &c);
+
+            if (c == 2) {
+                return 0;
+            }
+
+        } else if (dec == 2) {
+            for (int i = 0; i < con; i++) {
+                printf("nome canzone %s, artista %s, durata %d:%d\n\r",songs[i].nome, songs[i].artista, songs[i].minuti, songs[i].secondi);
+            }
+        } else {
+            return 0;
         }
-
-    } while (dec == 1);
+    } while (c != 0);
 
     realloc(songs, sizeof(struct song) * con);
-    printf("hai messo tutte le canzoni ora frasson ti sborrerà nel culo ecco l'elenco delle canzoni");
-
-    for (int i = 0; i < con; i++) {
-        printf("canzone %d titolo: %s, artista: %s, durata: %d:%d", i, songs[i].nome, songs[i].artista, songs[i].minuti, songs[i].secondi);
-    }
 
     return 0;
 }
